af8c820b635f02c9eb4502a7e253d93651372194
Arnold Juma <+>
2023-09-30 15:52:33 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input=document.getElementById('input');
+    function clickAlert(){
+        alert('I was Clicked');
+    }
+    input.addEventListener('click', clickAlert);
 }
